ef7bde32b3e70c2a6a942674814e07bd6bdce23f
Kevin Ellis <kellis.ca@gmail.com>
2012-05-28 22:11:23 -0400

    Add switching between combat and character page.

--- server/client/js/character-button.js
+++ server/client/js/character-button.js
@@ -1,6 +1,5 @@
 dungeon.CharacterButton = (function() {
 
-
   function characterButton_(characterData) {
     var template = $('character-template');
     var element = template.cloneNode(true);
@@ -13,6 +12,8 @@ dungeon.CharacterButton = (function() {
     // TODO(kellis): Option to sensor info for the bad guys.
     setCharacterAttribute_(element, 'level', characterData.level);
     setCharacterAttribute_(element, 'class', characterData.charClass);
+    element.addEventListener('click', showCharacterSheet_);
+    $('close-button').addEventListener('click', showCombatSheet_);
     return element;
   }
 
@@ -21,6 +22,17 @@ dungeon.CharacterButton = (function() {
     if (attributeValue == undefined)
       attributeValue = '?';
     characterElement.getElementsByClassName(className)[0].textContent = attributeValue;
+  }
+
+  function showCharacterSheet_() {
+    // TODO(kellis): initialize page content.
+    $('combat-page').hidden = true;
+    $('character-page').hidden = false;
+  }
+
+  function showCombatSheet_() {
+    $('character-page').hidden = true;
+    $('combat-page').hidden = false;
   }
 
   return characterButton_;
