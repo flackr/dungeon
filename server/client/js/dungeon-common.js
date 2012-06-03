0743ee54eed89c2423c2b8ba8f9a50372044e340
Robert Flack <flackr@gmail.com>
2012-06-03 19:25:53 -0400

    Add image tiles, allow painting tiles by type and using mousemove events.

--- server/client/js/dungeon-common.js
+++ server/client/js/dungeon-common.js
@@ -2,21 +2,69 @@
  * Dungeon common game class.
  */
 
+/**
+ * Extends the |base| prototype with |derived| by merging the two objects into
+ * a new object.
+ */
+function extend(base, derived) {
+  var proto = {};
+  for (var i in base)
+    proto[i] = base[i];
+  for (var i in derived)
+    proto[i] = derived[i];
+  return proto;
+}
+
 var dungeon = {};
 
-dungeon.Game = function() {
+dungeon.EventSource = function() {
+  this.listeners = {};
 };
 
-dungeon.Game.prototype = {
+dungeon.EventSource.prototype = {
+  addEventListener: function(type, listener) {
+    if (!this.listeners[type])
+      this.listeners[type] = [];
+    this.listeners[type].push(listener);
+  },
+
+  dispatchEvent: function(type /*, args... */) {
+    if (!this.listeners[type])
+      return;
+    for (var i = this.listeners[type].length - 1; i >=0; i--) {
+      this.listeners[type][i].apply(this,
+          Array.prototype.slice.call(arguments, 1));
+    }
+  },
+
+  removeEventListener: function(type, listener) {
+    if (!this.listeners[type])
+      return;
+    for (var i = this.listeners[type].length - 1; i >=0; i--) {
+      if (this.listeners[type][i] == listener)
+        this.listeners[type].splice(i, 1);
+    }
+  },
+};
+
+dungeon.Game = function() {
+  dungeon.EventSource.apply(this);
+};
+
+dungeon.Game.prototype = extend(dungeon.EventSource ,{
   initialize: function() {
     var mapWidth = 60;
     var mapHeight = 60;
     this.events = [];
+    this.mapTiles = [
+      {'src': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wGAxUZHk8/2VoAAAAdaVRYdENvbW1lbnQAAAAAAENyZWF0ZWQgd2l0aCBHSU1QZC5lBwAAAA1JREFUCNdjYGBg+A8AAQQBAKTgrDEAAAAASUVORK5CYII='},
+      {'src': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wGAxUVG5PgYtkAAAAdaVRYdENvbW1lbnQAAAAAAENyZWF0ZWQgd2l0aCBHSU1QZC5lBwAAABdJREFUCNdj1FzG/vPnT6afP3+y8jMDADNeBsdmI5bVAAAAAElFTkSuQmCC'},
+    ];
     this.map = [];
     for (var i = 0; i < mapHeight; i++) {
       this.map.push([]);
       for (var j = 0; j < mapWidth; j++) {
-        this.map[i].push(false);
+        this.map[i].push(1);
       }
     }
     this.characterRegistry = [];
@@ -78,7 +126,7 @@ dungeon.Game.prototype = {
    */
   update: function() {
   },
-};
+});
 
 // If being included from NodeJS, add dungeon.Game to exports.
 if (typeof exports == 'object' && exports) {
